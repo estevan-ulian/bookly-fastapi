087df31a7f0cbad93728f7b61a7ba8e82f330b3c
Estevan Ulian <estevan.ulian@gmail.com>
2026-03-22 13:50:47 -0300

    refactor: update database implementation

--- src/db/main.py
+++ src/db/main.py
@@ -1,28 +1,23 @@
-from sqlmodel import create_engine, SQLModel
+from sqlmodel import SQLModel
+from typing import AsyncGenerator
 from sqlmodel.ext.asyncio.session import AsyncSession
-from sqlalchemy.ext.asyncio import AsyncEngine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 from src.config import Config
 
-async_engine = AsyncEngine(
-    create_engine(
-        url=Config.DATABASE_URL,
-        echo=True,
-    )
+async_engine = create_async_engine(
+    url=Config.DATABASE_URL,
+    echo=True,
 )
 
 
 async def init_db():
     async with async_engine.begin() as connection:
-        from src.books.models import BookModel  # noqa: F401
-        from src.auth.models import UserModel  # noqa: F401
-
         await connection.run_sync(SQLModel.metadata.create_all)
 
 
-async def get_session() -> AsyncSession:
-    Session = sessionmaker(
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    Session = async_sessionmaker(
         bind=async_engine,
         class_=AsyncSession,
         expire_on_commit=False,
