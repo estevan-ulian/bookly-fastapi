7ac35445c1bc8db2aa4d01d650d8467774897b5f
Estevan Ulian <estevan.ulian@gmail.com>
2026-03-22 12:33:30 -0300

    refactor: consolidate token handling into a base TokenBearer class

--- src/auth/dependencies.py
+++ src/auth/dependencies.py
@@ -5,14 +5,14 @@ from fastapi.security.http import HTTPAuthorizationCredentials
 from .utils import decode_token
 
 
-class AccessTokenBearer(HTTPBearer):
+class TokenBearer(HTTPBearer):
     def __init__(self, auto_error=True):
         super().__init__(auto_error=auto_error)
 
     async def __call__(self, request: Request) -> HTTPAuthorizationCredentials | None:
         creds = await super().__call__(request)
 
-        token = creds.credentials  # type: ignore
+        token = creds.credentials
 
         token_data = decode_token(token)
 
@@ -22,14 +22,34 @@ class AccessTokenBearer(HTTPBearer):
                 detail="Invalid or expired token"
             )
 
-        if token_data['refresh']:  # type: ignore
-            raise HTTPException(
-                status_code=status.HTTP_403_FORBIDDEN,
-                detail="Please provide an access token, not a refresh token"
-            )
+        self.verify_token_data(token_data)
 
-        return token_data  # type: ignore
+        return token_data
 
     def token_valid(self, token: str) -> bool:
         token_data = decode_token(token)
         return True if token_data is not None else False
+
+    def verify_token_data(self, token_data: dict):
+        raise NotImplementedError(
+            "Please override this method in child classes.")
+
+
+class AccessTokenBearer(TokenBearer):
+
+    def verify_token_data(self, token_data: dict) -> None:
+        if token_data and token_data['refresh']:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="Please provide an access token."
+            )
+
+
+class RefreshTokenBearer(TokenBearer):
+
+    def verify_token_data(self, token_data: dict) -> None:
+        if token_data and not token_data['refresh']:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="Please provide an refresh token"
+            )
